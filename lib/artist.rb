841bc96230a83f6ddb59b327ad2aa33a1048e33b
Natasha Chernis <natashachernis@gmail.com>
2019-04-10 14:31:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,8 +2,12 @@ require 'pry'
 
 class Artist
 
+<<<<<<< HEAD
   attr_accessor :name
   attr_reader :songs
+=======
+  attr_accessor :songs, :name
+>>>>>>> b1905b275a561170b66b360419621ab1abfe81e1
   @@artists = []
 
   def initialize(name)
@@ -14,12 +18,21 @@ class Artist
 
   def add_song(song)
     song.artist = self
+<<<<<<< HEAD
     self.songs << song
+=======
+    @songs << song
+>>>>>>> b1905b275a561170b66b360419621ab1abfe81e1
   end
 
   def add_song_by_name(title)
     song = Song.new(title)
+<<<<<<< HEAD
     add_song(song)
+=======
+    song.artist = self
+    self.songs << song
+>>>>>>> b1905b275a561170b66b360419621ab1abfe81e1
   end
 
   def self.song_count
--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,15 @@ class Song
 
   @@all = []
 
+<<<<<<< HEAD
   def initialize(name)
     @name = name
     @@all << self
+=======
+  def new(name)
+    @name = name
+    @@songs << self
+>>>>>>> b1905b275a561170b66b360419621ab1abfe81e1
   end
 
   def self.all
@@ -13,6 +19,10 @@ class Song
   end
 
   def artist_name
+<<<<<<< HEAD
     self.artist ? self.artist.name : nil
+=======
+    nil unless self.artist.name
+>>>>>>> b1905b275a561170b66b360419621ab1abfe81e1
   end
 end
